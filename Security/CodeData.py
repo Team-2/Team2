3a95509d942d118f2eb1ceccab7938abc71d82b7
andreyvaran <moscow.russia.mai@gmail.com>
2021-09-26 02:27:08 +0300

    some comment

--- Security/CodeData.py
+++ Security/CodeData.py
@@ -2,7 +2,7 @@ def some_encryption(data):
     #todo encription data
     return data
 
-
+# Это заглушка , на место которой можно установить настоящее шифрование
 def some_decryption(data):
     #todo decryption data
     return data
